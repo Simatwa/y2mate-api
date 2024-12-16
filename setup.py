197106a0bd5bb61fb8a726edc4954789763a4a1f
Simatwa <smartwacaleb@gmail.com>
2024-12-16 18:57:16 +0300

    chore: Bump version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = "1.2.2"
+version = "1.2.3"
 info = "Download youtube videos and audios by title or link"
 author = "Smartwa"
 repo = "https://github.com/Simatwa/y2mate-api"
